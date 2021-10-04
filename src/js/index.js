14bf526bc7cac91b4aae486a85130e25cba12095
Bjorn Loyens <bjorn.loyens@gmail.com>
2021-10-04 11:08:33 +0200

    loader animation

--- src/js/index.js
+++ src/js/index.js
@@ -9,15 +9,29 @@ const searchField = document.getElementById("searchbar");
 const form = document.querySelector("form");
 const cardTemplate = document.querySelector("template").innerHTML;
 const grid = document.getElementById("cards");
+const loader = document.querySelector(".loading");
 
 /**
  * EVENTS
  */
+function showLoader() {
+  grid.innerHTML = "";
+  loader.style.display = "block";
+  grid.style.display = "none";
+  searchfield.disabled = true;
+}
+
+function stopLoader() {
+  loader.style.display = "none";
+  grid.style.display = "block";
+  searchfield.disabled = false;
+}
 
 form.onsubmit = function (e) {
   e.preventDefault();
+  showLoader;
   const { value } = searchField;
-  if (value.length > 3) {
+  if (value.length > 2) {
     httpClient(`/search/photos?&query=${value}`).then((response) => {
       grid.innerHTML = response.data.results
         .map((unsplashObj) =>
@@ -29,4 +43,5 @@ form.onsubmit = function (e) {
         .join("");
     });
   }
+  stopLoader;
 };
