295a461cfc5ae2b8ad71315b5683c78d065ff103
Bjorn Loyens <bjorn.loyens@gmail.com>
2021-10-07 20:49:21 +0200

    preload images done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import "../css/style.scss";
-import { httpClient } from "./helpers";
+import { httpClient, preloadImage, preloadImages } from "./helpers";
 
 /**
  * VARS
@@ -9,53 +9,32 @@ const searchField = document.getElementById("searchbar");
 const form = document.querySelector("form");
 const cardTemplate = document.querySelector("template").innerHTML;
 const grid = document.getElementById("cards");
-const loader = document.querySelector(".loading");
+const loader = document.querySelector(".loader");
 
 /**
  * EVENTS
  */
-function showLoader() {
-  grid.innerHTML = "";
-  loader.style.display = "block";
-  grid.style.display = "none";
-  searchField.disabled = true;
-  if (document.readyState === "complete") {
-    console.log("complete");
-    setTimeout(() => {
-      loader.style.display = "none";
-      grid.style.display = "grid";
-      searchField.disabled = false;
-      searchField.value = "";
-    }, 6000);
-  }
-  // setTimeout(() => {
-  //   loader.style.display = "none";
-  //   grid.style.display = "grid";
-  //   searchField.disabled = false;
-  // }, 10000);
-}
 
-// const stopLoader = () => {
-//   loader.style.display = "none";
-//   grid.style.display = "grid";
-//   searchField.disabled = false;
-// };
-
-form.onsubmit = function (e) {
+form.onsubmit = async function (e) {
   e.preventDefault();
-  showLoader();
   const { value } = searchField;
   if (value.length > 2) {
-    httpClient(`/search/photos?&query=${value}`).then((response) => {
-      grid.innerHTML = response.data.results
-        .map((unsplashObj) =>
-          cardTemplate
-            .replace("#CARD_IMAGE_URL", unsplashObj.urls.thumb)
-            // .replace("#CARD_PROFILE",)
-            .replaceAll("#CARD_TEXT", unsplashObj.alt_description)
-        )
-        .join("");
-    });
+    const response = await httpClient(`/search/photos?&query=${value}`);
+    loader.classList.add("loading");
+    searchField.disabled = true;
+    await preloadImages(
+      response.data.results.map((unsplashObj) => unsplashObj.urls.thumb)
+    );
+    loader.classList.remove("loading");
+    searchField.disabled = false;
+    searchField.value = "";
+    grid.innerHTML = response.data.results
+      .map((unsplashObj) =>
+        cardTemplate
+          .replace("#CARD_IMAGE_URL", unsplashObj.urls.thumb)
+          // .replace("#CARD_PROFILE",)
+          .replaceAll("#CARD_TEXT", unsplashObj.alt_description)
+      )
+      .join("");
   }
-  // stopLoader;
 };
